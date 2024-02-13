1d590b6d72ce2673bd6e12a687cd1fe8a04c08f4
Klaes Sørensen <klsoe20@student.sdu.dk>
2024-02-13 14:33:21 +0100

    qol

--- Semester4-Algoritmer-DataStrukturer/src/main/java/Execution/Main.java
+++ Semester4-Algoritmer-DataStrukturer/src/main/java/Execution/Main.java
@@ -3,6 +3,8 @@ package Execution;
 import Opgaver_Uge_6.CycleCounter;
 import Opgaver_Uge_6.PermutationGenerator;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
         opgaverUge6();
@@ -20,6 +22,15 @@ public class Main {
         System.out.println("\nCycle Counter: ");
         CycleCounter cycleCounter = new CycleCounter();
         cycleCounter.classDemonstration();
+
+        long timeStart = System.currentTimeMillis();
         cycleCounter.EB2(10000000, 16);
+        long totalTime = System.currentTimeMillis() - timeStart;
+        String toPrint = String.format("%02d min, %02d sec",
+                TimeUnit.MILLISECONDS.toMinutes(totalTime),
+                TimeUnit.MILLISECONDS.toSeconds(totalTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(totalTime))
+        );
+        System.out.println("Time: " + toPrint);
     }
 }
